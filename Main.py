689ea6e26f17bb7d79145aebd523792cf84b2ffc
Isiah61706 <isiah617@icloud.com>
2025-11-03 11:34:02 +0000

    Fixed Lines and : placement in Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
+import random
+
 class die: 
     def __init__(self, sides=6):
         self.sides = sides 
-    def roll(self)
+    def roll(self):
         return random.randint(1,  self.sides)
 
 
 class DiceGame:
-    def __init__(self)
+    def __init__(self):
         self.die1 = die()
         self.die2 = die()
     def evaluate_roll(self, total):
@@ -14,15 +16,15 @@ class DiceGame:
             return "Win"
         elif total in [2,3,12]:
             return "Lose"
-        else":
+        else:
             return "Roll Again"
     
-    def play_round(self)
-    roll1 = self.die1.roll()
-    roll2 = self.die2.roll()
-    total = roll1 + roll2
-    result = self.evaluate_roll(total)
-    return roll1. roll2. total, result
+    def play_round(self):
+        roll1 = self.die1.roll()
+        roll2 = self.die2.roll()
+        total = roll1 + roll2
+        result = self.evaluate_roll(total)
+        return roll1. roll2. total, result
 
 def main():
     game = DiceGame()
@@ -44,9 +46,5 @@ def main():
             break
         else:
             print("Invalid choice. Please enter 1 or 2.")
-
-
 if __name__ == "__main__":
     main()
-
-import random
